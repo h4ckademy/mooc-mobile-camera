372f30c79c5ddbd48dc30e5c4ece8b536cd1a4b1
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2016-10-09 22:38:38 +0200

    usando el canvas

--- platforms/browser/www/js/camera.js
+++ platforms/browser/www/js/camera.js
@@ -25,8 +25,19 @@ var app = {
   },
 
   fotoTomada: function(imageURI){
-    var image = document.querySelector('#foto');
-    image.src = imageURI;
+    var img = document.createElement('img');
+    img.onload = function(){
+      app.pintarFoto(img);
+    }
+    img.src = imageURI;
+  },
+
+  pintarFoto: function(img){
+    var canvas = document.querySelector('#foto');
+    var context = canvas.getContext('2d');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    context.drawImage(img, 0, 0, img.width, img.height);
   },
 
   errorAlTomarFoto: function(message) {
