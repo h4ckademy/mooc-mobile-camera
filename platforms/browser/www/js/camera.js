fab0f46bea6b6f39a2529217edac467f337ad8a3
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2016-10-09 23:25:02 +0200

    filtros

--- platforms/browser/www/js/camera.js
+++ platforms/browser/www/js/camera.js
@@ -1,16 +1,27 @@
 var app = {
   inicio: function() {
     this.iniciaFastClick();
-    this.iniciaBoton();
+    this.iniciaBotones();
   },
 
   iniciaFastClick: function () {
     FastClick.attach(document.body);
   },
 
-  iniciaBoton: function(){
+  iniciaBotones: function(){
     var buttonAction = document.querySelector('#button-action');
     buttonAction.addEventListener('click', this.tomarFoto);
+
+    var filterButtons = document.querySelectorAll('.button-filter');
+    filterButtons[0].addEventListener('click', function(){
+      app.aplicaFiltro('gray');
+    });
+    filterButtons[1].addEventListener('click', function(){
+      app.aplicaFiltro('negative');
+    });
+    filterButtons[2].addEventListener('click', function(){
+      app.aplicaFiltro('sepia');
+    });
   },
 
   tomarFoto: function() {
@@ -42,9 +53,20 @@ var app = {
 
   errorAlTomarFoto: function(message) {
     console.log('Fallo al tomar foto o toma cancelada: ' + message);
+  },
+
+  aplicaFiltro: function(filterName){
+    var canvas = document.querySelector('#foto');
+    var context = canvas.getContext('2d');
+    imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+    effects[filterName](imageData.data);
+
+    context.putImageData(imageData, 0, 0);
   }
 };
 
+var imageData;
 if ('addEventListener' in document) {
   document.addEventListener('DOMContentLoaded', function() {
     app.inicio();
