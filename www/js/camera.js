17bf0be20baa352099ab1ccd667f430942d065ec
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2016-10-09 21:50:39 +0200

    tomar una foto

--- www/js/camera.js
+++ www/js/camera.js
@@ -1,17 +1,36 @@
 var app = {
   inicio: function() {
     this.iniciaFastClick();
+    this.iniciaBoton();
   },
 
   iniciaFastClick: function () {
     FastClick.attach(document.body);
   },
 
-  bindEvents: function() {
-    document.addEventListener('deviceready', this.onDeviceReady, false);
+  iniciaBoton: function(){
+    var buttonAction = document.querySelector('#button-action');
+    buttonAction.addEventListener('click', this.tomarFoto);
   },
-  onDeviceReady: function() {
 
+  tomarFoto: function() {
+    var opciones = {
+      quality: 50,
+      destinationType: Camera.DestinationType.FILE_URI,
+      targetWidth: 300,
+      targetHeight: 300,
+      correctOrientation: true
+    };
+    navigator.camera.getPicture(app.fotoTomada, app.errorAlTomarFoto, opciones);
+  },
+
+  fotoTomada: function(imageURI){
+    var image = document.querySelector('#foto');
+    image.src = imageURI;
+  },
+
+  errorAlTomarFoto: function(message) {
+    console.log('Fallo al tomar foto o toma cancelada: ' + message);
   }
 };
 
